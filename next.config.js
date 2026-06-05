3b09dff583dac205b506137e46a784be041a17f1
barbara452 <barbaragomes@pwrgestao.com>
2026-06-05 18:21:11 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-        swcMinify: true,
-      }
+  reactStrictMode: true,
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      stream: false,
+      path: false,
+      crypto: false,
+      buffer: false,
+    }
+    return config
+  },
+}
 
-      module.exports = nextConfig
+module.exports = nextConfig
